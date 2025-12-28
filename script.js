e393a33e8612282347113d6e4d992936b66d6424
unknown <dionahza15@gmail.com>
2025-12-28 09:30:44 +0700

    fix navbar

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   fetch('/components/navbar.html')
-    .then(res => {
-      if (!res.ok) throw new Error('Navbar not found');
-      return res.text();
-    })
+    .then(res => res.text())
     .then(data => {
       document.getElementById('navbar').innerHTML = data;
-    })
-    .catch(err => console.error(err));
+    });
 });
+
 
 //////// jangan diubah yang diatas ////////
 // Hover efek follow mouse di circle
